62ceeed3f54da4e8d94c13154e7708828c96bf9e
Manuel Ventura <Nelo.manuel.790@gmail.com>
2023-01-22 06:26:35 +0100

    Refactoração do codigo melhoria no toggle de light/dak mode

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,10 +1,15 @@
 let menu = document.querySelector('.menu');
-let toggle = document.querySelector('.switch');
+let toggle = document.querySelector('.mdi-36px');
 let btnmenu = document.querySelector('.fa-bars');
 let menuItem = document.querySelectorAll('.menuItem');
 
-toggle.addEventListener('click', event=>{
-	toggle.classList.toggle(toggle.classList.replace('fa-toggle-on', 'fa-toggle-off'))
+toggle.addEventListener('click', ()=>{
+	console.log('Click')
+	if(toggle.classList.contains('mdi-toggle-switch')){
+		toggle.classList.replace('mdi-toggle-switch', 'mdi-toggle-switch-off-outline')
+	}else{
+		toggle.classList.replace('mdi-toggle-switch-off-outline','mdi-toggle-switch')
+	}
 })
 
 btnmenu.addEventListener('click', event=>{
