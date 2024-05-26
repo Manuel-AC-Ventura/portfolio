1d2728f4ef27a671423ca6452b61130b44171962
Manuel Ventura <nelo.manuel.790@gmail.com>
2024-05-26 16:21:41 +0100

    chore: Update footer text to Portuguese in Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ export const Footer = () => {
     <section id="Contact" className={`w-full py-12 md:py-24 lg:py-32 ${theme === 'dark' ? 'bg-gray-800' : 'bg-gray-100'}`}>
       <div className="container grid items-center justify-center gap-6 px-4 md:px-6 lg:grid-cols-2 lg:gap-10">
         <div className="space-y-4">
-          <h2 className={`text-3xl font-bold tracking-tighter md:text-4xl ${theme === 'dark' ? 'text-white' : 'text-gray-900'}`}>Get in Touch</h2>
+          <h2 className={`text-3xl font-bold tracking-tighter md:text-4xl ${theme === 'dark' ? 'text-white' : 'text-gray-900'}`}>Entrar em contato</h2>
           <p className={`max-w-[600px] md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed ${theme === 'dark' ? 'text-gray-400' : 'text-gray-500'}`}>
             I'd love to hear from you! Feel free to reach out with any questions or inquiries.
           </p>
