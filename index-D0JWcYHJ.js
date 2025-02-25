e39d23ec7240b94bc4c326cefc0d8f8ecc35bbbc
Manuel Ventura <nelo.manuel.790@gmail.com>
2025-02-25 09:52:36 +0100

    remove:dependencies

--- index-D0JWcYHJ.js
+++
@@ -1,5 +0,0 @@
-const resendKey = process.env.RESEND_KEY;
-if (!resendKey) {
-    throw new Error("Missing API key. Pass it to the constructor `new Resend('re_123')`");
-}
-const resend = new Resend(resendKey);
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,44 +1,11 @@
 import { Resend } from "resend";
 import { useTheme } from "@/context/themeContext";
 import { useTranslation } from "react-i18next";
-import { useState } from "react";
 
 export const Footer = () => {
   const { theme } = useTheme();
   const { t } = useTranslation();
-  const resendkey = process.env.RESEND_KEY;
-  const myEmail = process.env.My_EMAIL;
-  const resend = new Resend(resendkey);
-
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    message: ""
-  });
-
-  const handleChange = (e) => {
-    const { id, value } = e.target;
-    setFormData((prevData) => ({
-      ...prevData,
-      [id]: value
-    }));
-  };
-
-  const sendEmail = async (e) => {
-    e.preventDefault();
-    try {
-      await resend.emails.send({
-        from: myEmail,
-        to: myEmail,
-        subject: `Message from ${formData.name}`,
-        text: formData.message,
-      });
-      alert("Email sent successfully!");
-    } catch (error) {
-      console.error("Error sending email:", error);
-      alert("Failed to send email.");
-    }
-  };
+  
 
   return (
     <section
@@ -58,7 +25,7 @@ export const Footer = () => {
             {t("footer.contact_description")}
           </p>
         </div>
-        <form className="space-y-4" onSubmit={sendEmail}>
+        <form className="space-y-4">
           <div className="space-y-2">
             <label
               className={`text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 ${theme === "dark" ? "text-white" : "text-gray-900"}`}
@@ -70,8 +37,6 @@ export const Footer = () => {
               className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
               id="name"
               placeholder={t("footer.name_placeholder")}
-              value={formData.name}
-              onChange={handleChange}
             />
           </div>
           <div className="space-y-2">
@@ -85,8 +50,6 @@ export const Footer = () => {
               className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
               id="email"
               placeholder={t("footer.email_placeholder")}
-              value={formData.email}
-              onChange={handleChange}
             />
           </div>
           <div className="space-y-2">
@@ -100,8 +63,6 @@ export const Footer = () => {
               className="flex h-24 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
               id="message"
               placeholder={t("footer.message_placeholder")}
-              value={formData.message}
-              onChange={handleChange}
             />
           </div>
           <button
